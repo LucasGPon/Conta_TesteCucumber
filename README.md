475cf3800d74755ec09028b6c3be493bac0e77ea
Lucas Gaspar <211701@facens.br>
2022-10-23 12:46:52 -0300

    Projeto finalizado

---
+++ README.md
@@ -0,0 +1,34 @@
+# Conta Teste (Cucumber)
+
+## Descrição
+
+Aplicação desenvolvida em JAVA para a criação de um cenario de teste Cucumber, explorando sua interface de criação de códigos e resultados.
+
+### Pré requisitos
+
+Antes de começar, você precisará das seguintes ferramentas instaladas: [git](https://git-scm.com/) e [JDK 8](https://www.oracle.com/br/java/technologies/javase/javase8-archive-downloads.html)
+<br>
+Editores de código [Eclipse](https://www.eclipse.org/downloads/) ou [Netbeans](https://netbeans.apache.org/download/nb14/nb14.html)
+
+```
+
+# clone o repositório
+$ git clone https://github.com/LucasGPon/Conta_TesteCucumber.git
+
+```
+
+### Funcionamento
+
+Após clonar o projeto e abri-lo com a IDE, basta acessar o caminho src/test/java e executar o arquivo conta_bdd.feature.
+a solução do projeto se encontra no arquivo conta.java.
+
+### Tecnologias
+
+Para construir o projeto foi utilizado as seguintes ferramentas
+- [Eclipse](https://www.eclipse.org/)
+- [Java](https://www.java.com/pt-BR/)
+- [Cucumber](https://mvnrepository.com/artifact/io.cucumber/cucumber-java/7.8.1)
+
+### Autor
+
+- Lucas Gaspar Ponciano
--- src/test/java/conta.java
+++ src/test/java/conta.java
@@ -3,39 +3,68 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class conta {
+	private int saldo;
+	private String cliente;
+
+	/**
+	 * @param saldo Recebe saldo do cliente especial -200
+	 * 
+	 */
 	@Given("Um cliente especial com saldo atual de {int} reais")
-	public void um_cliente_especial_com_saldo_atual_de_reais(Integer int1) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+	public void um_cliente_especial_com_saldo_atual_de_reais(Integer saldo) {
+		cliente = "especial";
+		this.saldo = saldo;
 	}
 
+	/**
+	 * 
+	 * @param valorSacar Recebe valor a ser sacado pelo cliente especial
+	 */
 	@When("for solicitado um saque no valor de {int} reais")
-	public void for_solicitado_um_saque_no_valor_de_reais(Integer int1) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+	public void for_solicitado_um_saque_no_valor_de_reais(Integer valorSacar) {
+		if (cliente == "especial") {
+			saldo -= valorSacar;
+		}
 	}
 
+	/**
+	 * 
+	 * @param saldoAtual Atualiza saldo da conta do cliente
+	 */
 	@Then("deve efetuar o saque e atualizar o saldo da conta para {int} reais")
-	public void deve_efetuar_o_saque_e_atualizar_o_saldo_da_conta_para_reais(Integer int1) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+	public void deve_efetuar_o_saque_e_atualizar_o_saldo_da_conta_para_reais(Integer saldoAtual) {
+		if (saldo == saldoAtual) {
+			System.out.println("Saque realizado com sucesso. " + "\n Saldo " + saldoAtual);
+		}
 	}
 
+	/**
+	 * 
+	 * @param saldo Saldo cliente comum
+	 */
 	@Given("um cliente comum com saldo atual de {int} reais")
-	public void um_cliente_comum_com_saldo_atual_de_reais(Integer int1) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+	public void um_cliente_comum_com_saldo_atual_de_reais(Integer saldo) {
+		cliente = "comum";
+		this.saldo = saldo;
 	}
 
+	/**
+	 * 
+	 * @param valorSacar Recebe valor a ser sacado pelo cliente comum
+	 */
 	@When("solicitar um saque de {int} reais")
-	public void solicitar_um_saque_de_reais(Integer int1) {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+	public void solicitar_um_saque_de_reais(Integer valorSacar) {
+		if (cliente == "comum") {
+			não_deve_efetuar_o_saque_e_deve_retornar_a_mensagem_saldo_insuficiente();
+		}
 	}
 
+	/**
+	 * 
+	 *@param Retorna mensagem de saldo insuficiente
+	 */
 	@Then("não deve efetuar o saque e deve retornar a mensagem Saldo Insuficiente")
 	public void não_deve_efetuar_o_saque_e_deve_retornar_a_mensagem_saldo_insuficiente() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		System.out.println("Saldo Insuficiente");
 	}
 }
